ee1edf6d7c7f0070b81c208a4d0b22da624f901d
ita Dev <itajacy@gmail.com>
2025-08-25 19:34:43 -0300

    implementados addListener, removeListener e notifyCallback em ChangeState()

--- lib/controllers/change_state.dart
+++ lib/controllers/change_state.dart
@@ -2,14 +2,28 @@
 import 'package:pm_criando_gerenciamento_estado/contracts/observable.dart';
 
 class ChangeState implements Observable {
+  final List<void Function()> _callbacks = [];
+
   @override
   void addListener(void Function() callback) {
-    // TODO: implement addListener
+    // verifica se existe na lista
+    // se não existir, adiciona na lista
+    if (!_callbacks.contains(callback)) _callbacks.add(callback);
   }
 
   @override
   void removeListener(void Function() callback) {
-    // TODO: implement removeListener
+    // verifica se existe na lista
+    // se existir, remove da lista
+    if (_callbacks.contains(callback)) _callbacks.remove(callback);
   }
 
+  void notifyCallback() {
+    for (int i = 0; i < _callbacks.length; i++) {
+      // executando a função para atualizar o estado
+      // _callbacks[i]();
+      // ou assim:
+      _callbacks[i].call();
+    }
   }
+}
