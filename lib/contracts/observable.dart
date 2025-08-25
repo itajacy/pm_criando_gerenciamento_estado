d497d761618c406aa0c1b5c69eab5e208d70b075
ita Dev <itajacy@gmail.com>
2025-08-25 19:24:16 -0300

    criado contracts com observable, e changState() implements Observable

---
+++ lib/contracts/observable.dart
@@ -0,0 +1,7 @@
+abstract class Observable {
+
+  // callback -  função que não retorna nada, só é executada
+  void addListener(void Function() callback);
+  void removeListener(void Function() callback);
+  
+}
---
+++ lib/controllers/change_state.dart
@@ -0,0 +1,15 @@
+
+import 'package:pm_criando_gerenciamento_estado/contracts/observable.dart';
+
+class ChangeState implements Observable {
+  @override
+  void addListener(void Function() callback) {
+    // TODO: implement addListener
+  }
+
+  @override
+  void removeListener(void Function() callback) {
+    // TODO: implement removeListener
+  }
+
+  }
