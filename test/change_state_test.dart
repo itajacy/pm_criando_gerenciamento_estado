d4e2247e16229ed1464e6b568e1c780b09b255e2
itajacy TI02 <itajacy@bsd.com.br>
2025-09-03 16:35:11 -0300

    2.14 Testando gerenciamento de estado - inicio de \\lib\test

---
+++ test/change_state_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pm_criando_gerenciamento_estado/controllers/change_state.dart';
+
+void main() {
+  test("Should teste ChangeState", () {
+    final ChangeState changeState = ChangeState();
+  });
+}
