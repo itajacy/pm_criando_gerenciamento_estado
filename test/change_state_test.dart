0c5f81759d5d37cd7ce7bd1d7c5776c5fea1bc60
ita Dev <itajacy@gmail.com>
2025-09-11 21:25:48 -0300

    2.14 teste em grupo

--- test/change_state_test.dart
+++ test/change_state_test.dart
@@ -2,14 +2,37 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pm_criando_gerenciamento_estado/controllers/change_state.dart';
 
 void main() {
-
   //criando um teste unitario
-  test("Should teste ChangeState", () {
-    final _CounterChangeState changeState = _CounterChangeState();
-    changeState.increment();
+  group("Should teste ChangeState", () {
+    test("Should increment counter", () {
+      // Arrange - montar o nosso teste
+      final _CounterChangeState changeState = _CounterChangeState();
 
-    // aqui esperamos que o valor do counter seja 1
-    expect(changeState.counter, 1);
+      // Act - disparar a ação que queremos testar
+      changeState.increment();
+
+      // Assert - verificar se o resultado é o esperado
+      // aqui esperamos que o valor do counter seja 1
+      expect(changeState.counter, 1);
+    });
+    test("Should execute callback", () {
+      // Arrange - montar o nosso teste
+      bool callbackChanged = false;
+      final _CounterChangeState changeState = _CounterChangeState();
+
+      // Act - disparar a ação que queremos testar
+      changeState.addListener(() {
+        callbackChanged = true;
+      });
+      changeState.increment();
+
+      // Assert - verificar se o resultado é o esperado
+      // aqui esperamos que o valor do counter seja 1
+      expect(changeState.counter, 1);
+      expect(callbackChanged, true);
+    });
+
+
   });
 }
 
