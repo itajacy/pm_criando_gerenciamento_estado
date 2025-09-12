4a8c61438358b37f84b221f39e6afce5c5a415ce
itajacy TI02 <itajacy@bsd.com.br>
2025-09-12 14:19:15 -0300

    2.15 fim da parte de testes

--- test/observable_builder_teste.dart
+++ test/observable_builder_teste.dart
@@ -1,18 +1,34 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:pm_criando_gerenciamento_estado/builders/observable_builder.dart';
+import 'package:pm_criando_gerenciamento_estado/controllers/change_state.dart';
 
 void main() {
   //criando um teste unitario
   group("Should teste ObservableBuilder", () {
-    test("Should test increment builder", () {
-      // Arrange - montar o nosso teste
+    testWidgets("Should test increment builder", (widgetTests) async {
+      await widgetTests.pumpWidget(MaterialApp(home: const _CounterWidget()));
 
-      // Act - disparar a ação que queremos testar
+      final findText = find.text("Valor do Counter: 0");
 
-      // Assert - verificar se o resultado é o esperado
+      final findButton = find.byKey(const Key(incrementButtonKey));
+
+      expect(findText, findsOneWidget);
+
+      expect(findButton, findsOneWidget);
+
+      await widgetTests.tap(findButton);
+      // atualiza a tela
+      await widgetTests.pump();
+
+      final nextCounterText = find.text("Valor do Counter: 1");
+
+      expect(nextCounterText, findsOneWidget);
     });
   });
 }
+
+const incrementButtonKey = "increment_key";
 
 class _CounterWidget extends StatefulWidget {
   const _CounterWidget({super.key});
@@ -22,8 +38,34 @@ class _CounterWidget extends StatefulWidget {
 }
 
 class __CounterWidgetState extends State<_CounterWidget> {
+  final _counter = _ObservableCounter();
   @override
   Widget build(BuildContext context) {
-    return Text("Valor do Counter: 0");
+    return Column(
+      children: [
+        ObservableBuilder(
+          observable: _counter,
+          builder: (context, child) {
+            return Text("Valor do Counter: ${_counter.count}");
+          },
+        ),
+        ElevatedButton(
+          key: Key(incrementButtonKey),
+          onPressed: () {
+            _counter.increment();
+          },
+          child: const Text('Incrementar '),
+        ),
+      ],
+    );
+  }
+}
+
+class _ObservableCounter extends ChangeState {
+  int count = 0;
+
+  void increment() {
+    count++;
+    notifyCallback();
   }
 }
