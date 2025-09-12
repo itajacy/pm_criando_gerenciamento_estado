6a8bf34bb4c0bde3a51ab08c3d91c784eeeecb5b
ita Dev <itajacy@gmail.com>
2025-09-11 21:13:14 -0300

    2.14 primeiro teste unitario funcionando, no vscode clicar no botao Testing e mandar rodar e ver no debu console

--- test/change_state_test.dart
+++ test/change_state_test.dart
@@ -2,7 +2,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pm_criando_gerenciamento_estado/controllers/change_state.dart';
 
 void main() {
+
+  //criando um teste unitario
   test("Should teste ChangeState", () {
-    final ChangeState changeState = ChangeState();
+    final _CounterChangeState changeState = _CounterChangeState();
+    changeState.increment();
+
+    // aqui esperamos que o valor do counter seja 1
+    expect(changeState.counter, 1);
   });
+}
+
+class _CounterChangeState extends ChangeState {
+  int counter = 0;
+
+  void increment() {
+    counter++;
+    notifyCallback();
+  }
 }
