9e36cec4f82aa15bc43b2cfa0e44ff364d817083
ita Dev <itajacy@gmail.com>
2025-09-13 20:30:16 -0300

    2.16 - teste usando State Pattern, usando a classe BaseState com Initial, Loading, Success e Error State

--- test/state_observable_test.dart
+++ test/state_observable_test.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:pm_criando_gerenciamento_estado/controllers/state_observable.dart';
 
 void main() {
@@ -14,21 +16,84 @@ void main() {
       expect(counterState.state, 1);
     });
 
-    test("Should execute StateObservable callback when we increment counter", (){
-      // Arrange
-      bool isCallbackExecuted = false;
-      final counterState = StateObservable<int>(0);
+    test(
+      "Should execute StateObservable callback when we increment counter",
+      () {
+        // Arrange
+        bool isCallbackExecuted = false;
+        final counterState = StateObservable<int>(0);
 
-      // Act
-      void callback(){
-        isCallbackExecuted = true;
-      }
+        // Act
+        void callback() {
+          isCallbackExecuted = true;
+        }
 
-      counterState.addListener(callback);
-      counterState.state++;
+        counterState.addListener(callback);
+        counterState.state++;
 
-      // Assert
-      expect(isCallbackExecuted, true);
-    });
+        // Assert
+        expect(isCallbackExecuted, true);
+      },
+    );
   });
+  test("Should generate SuccessState when we call (getProducts)", () {
+    // Arrange
+    final productController = ProductController();
+    expect(productController.state, isA<InitialState>());
+    // Act
+    productController.getProducts();
+
+    // Assert
+    // isA<> verifica se o tipo é do tipo SuccessState
+    expect(productController.state, isA<SuccessState>());
+  });
+}
+
+abstract class BaseState {}
+
+// ou IddleState   estado inicial
+class InitialState extends BaseState {}
+
+//estado de carregamento
+class LoadingState extends BaseState {}
+
+// estado de sucesso
+// normalmente temos uma variável que representa o valor do estado T
+class SuccessState<T extends Object> extends BaseState {
+  final T data;
+  SuccessState({required this.data});
+}
+
+// estado de erro
+// normalmente temos uma variável que representa a mensagem de erro
+class ErrorState extends BaseState {
+  final String message;
+  ErrorState({required this.message});
+}
+
+class Product {
+  final int id;
+  final String name;
+  Product({
+    required this.id,
+    required this.name,
+  });
+}
+
+class ProductController extends StateObservable<BaseState> {
+  // alterando o estado inicial para InitialState, passando por dentro
+  // ProductController(super.state);
+  ProductController() : super(InitialState());
+
+  void getProducts() {
+    // quando iniciar a requisição, alteramos o estado para LoadingState
+    state = LoadingState();
+    // simulando uma requisição de rede
+    state = SuccessState(
+      data: [
+        Product(id: 1, name: "Primeiro produto"),
+        Product(id: 2, name: "Segundo produto"),
+      ],
+    );
+  }
 }
