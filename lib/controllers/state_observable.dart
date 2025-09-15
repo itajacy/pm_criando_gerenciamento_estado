851d6bc5f3232ed510d21c6fd7c8270e2d515ebb
ita Dev <itajacy@gmail.com>
2025-09-15 14:26:51 -0300

    2.17 - testando emissao de estados em sequencia

--- lib/controllers/state_observable.dart
+++ lib/controllers/state_observable.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:pm_criando_gerenciamento_estado/contracts/observable_state.dart';
 import 'package:pm_criando_gerenciamento_estado/controllers/change_state.dart';
 
 class StateObservable<T> extends ChangeState implements ObservableState<T> {
-   T _state;
+  T _state;
 
   @override
   T get state => _state;
@@ -14,4 +17,34 @@ class StateObservable<T> extends ChangeState implements ObservableState<T> {
   }
 
   StateObservable(this._state);
+}
+
+extension ObservableStream<T> on StateObservable<T> {
+  Stream<T> asStream() {
+    StreamController<T> streamController = StreamController<T>();
+
+    streamController.add(state); //InitialState
+
+    void callback() {
+      streamController.add(state); //LoadingState, --> SuccessState
+    }
+
+    addListener(callback);
+    return streamController.stream;
+  }
+}
+
+extension ObservableValueNotifier<T> on ValueNotifier<T> {
+  Stream<T> asStream() {
+    StreamController<T> streamController = StreamController<T>();
+
+    streamController.add(value); //InitialState
+
+    void callback() {
+      streamController.add(value); //LoadingState, --> SuccessState
+    }
+
+    addListener(callback);
+    return streamController.stream;
+  }
 }
--- test/state_observable_test.dart
+++ test/state_observable_test.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:pm_criando_gerenciamento_estado/controllers/state_observable.dart';
@@ -47,6 +48,56 @@ void main() {
     // isA<> verifica se o tipo é do tipo SuccessState
     expect(productController.state, isA<SuccessState>());
   });
+
+  test("Should generate states in sequence", () {
+    final productController = ProductController();
+    expect(
+      productController.asStream(),
+      emitsInOrder([
+        isInstanceOf<InitialState>(),
+        isInstanceOf<LoadingState>(),
+        isInstanceOf<SuccessState<List<Product>>>(),
+      ]),
+    );
+    productController.getProducts();
+  });
+  test("Should generate states in sequence when we get error", () {
+    final productController = ProductController();
+    expect(
+      productController.asStream(),
+      emitsInOrder([
+        isInstanceOf<InitialState>(),
+        isInstanceOf<LoadingState>(),
+        isInstanceOf<ErrorState>(),
+      ]),
+    );
+    productController.generateError();
+  });
+  test("Should generate states in sequence when we get error2", () {
+    final productController = ProductController();
+    expect(
+      productController.asStream(),
+      emitsInOrder([
+        isInstanceOf<InitialState>(),
+        isInstanceOf<LoadingState>(),
+        isInstanceOf<SuccessState>(),
+        isInstanceOf<LoadingState>(),
+        isInstanceOf<ErrorState>(),
+      ]),
+    );
+    productController.getProducts();
+    productController.generateError();
+  });
+
+  test("Testing ValueNotifier", () {
+    final valueNotifier = ValueNotifier<int>(0); //0
+
+    expect(valueNotifier.asStream(), emitsInOrder([0, 1, 2]));
+
+    valueNotifier.value++; //1
+    valueNotifier.value++; //2
+  });
+
 }
 
 abstract class BaseState {}
@@ -95,5 +146,16 @@ class ProductController extends StateObservable<BaseState> {
         Product(id: 2, name: "Segundo produto"),
       ],
     );
+  }
+
+  void generateError() {
+    // quando iniciar a requisição, alteramos o estado para LoadingState
+    state = LoadingState();
+    // simulando uma requisição de rede
+    try {
+      throw Exception();
+    } catch (e) {
+      state = ErrorState(message: "Erro ao buscar produtos");
+    }
   }
 }
